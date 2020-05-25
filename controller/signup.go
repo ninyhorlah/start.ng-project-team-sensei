f34c1dda86b2f7807f44ca93c2090de59dfdf592
youthtrouble <47859940+youthtrouble@users.noreply.github.com>
2020-05-25 22:59:02 +0100

    feat: implements user signup and redirect

--- controller/signup.go
+++ controller/signup.go
@@ -1,51 +1,49 @@
 package controller
 
 import (
-	"sensei-poultry-management/model"
-	"sensei-poultry-management/views"
 	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
-	"html/template"
+	"sensei-poultry-management/model"
+	"sensei-poultry-management/views"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 const hashCost = 8
 
-
 //Signup sWitches between requests get and post in SignupGet and SignupPost
-func Signup(w http.ResponseWriter, r*http.Request) {
+func Signup(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		SignupGet(w, r)
 	case "POST":
 		SignupPost(w, r)
-		
+
 	}
 }
 
 //SignupGet renders the signup ppage
-func SignupGet(w http.ResponseWriter, r *http.Request){
-	tmpl, _ :=template.ParseFiles("./templates/signup.html")
+func SignupGet(w http.ResponseWriter, r *http.Request) {
+	tmpl, _ := template.ParseFiles("./templates/signup.html")
 	tmpl.Execute(w, nil)
 }
 
-
 // SignupPost registers a new user in the database
-func SignupPost(w http.ResponseWriter, r *http.Request){
+func SignupPost(w http.ResponseWriter, r *http.Request) {
 	// Parse and decode the request body into a new `Credentials` instance
 	creds := &views.Credentials{
 		Username: r.FormValue("Username"),
-		Email: r.FormValue("Email"),
+		Email:    r.FormValue("Email"),
 		Password: r.FormValue("Password"),
 	}
-	
+
 	// Salt and hash the password using the bcrypt algorithm
 	// The second argument is the cost of hashing, which we arbitrarily set as 8 (this value can be more or less, depending on the computing power you wish to utilize)
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), hashCost)
 	username := creds.Username
-	email :=creds.Email
+	email := creds.Email
 	passkey := string(hashedPassword)
 	// Next, insert the username, along with the email hashed , password into the database
 	if err = model.CreateUser(username, email, passkey); err != nil {
@@ -54,7 +52,12 @@ func SignupPost(w http.ResponseWriter, r *http.Request){
 		log.Fatal(err)
 		return
 	}
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(creds.Username)
+
 	// We reach this point if the credentials were correctly stored in the database, and the default status of 200 is sent back
+	if r.Method == "POST" {
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(creds.Username)
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+	}
+
 }
