b9821b6d2695e353e3afe307472072e108e0fe00
youthtrouble <47859940+youthtrouble@users.noreply.github.com>
2020-05-26 19:22:08 +0100

    feat: implements signup and merges singin properly

--- model/connect.go
+++ model/connect.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-
+	//"strconv"
 	"github.com/joho/godotenv"
 )
 
@@ -14,26 +14,33 @@ var db *sql.DB
 
 //InitDB establishes the database connection
 func InitDB() *sql.DB {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	err:= godotenv.Load()
+	if err!= nil{
+		log.Println("Error loading .env file")
 	}
-
 	host := os.Getenv("host")
-	user := os.Getenv("user")
-	password := os.Getenv("password")
-	dbname := os.Getenv("dbname")
+	//port,_ := strconv.Atoi(os.Getenv("dbport"))
+	user:= os.Getenv("user")
+	password:= os.Getenv("password")
+	dbname:= os.Getenv("dbname")
+	//connectionURL := os.Getenv("DATABASE_URL")
+	
+
 	// Connect to the postgres db
 	//you might have to change the connection string to add your database credentials
-	psqlinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, 5432, user, password, dbname)
+	psqlinfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, 5432 , user ,password, dbname)
+	//db, err = sql.Open("postgres", connectionURL)
 	db, err = sql.Open("postgres", psqlinfo)
+
+	log.Println("connecting to db")
 	if err != nil {
+		log.Println("Database could not connect",err)
 		panic(err)
 	}
 	err = db.Ping()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) 
 	}
-	fmt.Println("Connected to database")
+	log.Println("Connected to database")
 	return db
 }
--- model/createValidate.go
+++ model/createValidate.go
@@ -3,7 +3,7 @@ package model
 // CreateUser creates a new user
 func CreateUser(username, email, password string) error {
 	// before creating user check if the user name exits
-	if _, err := db.Query("INSERT INTO forthebirds (username, email, password) VALUES ($1,$2,$3)", username, email, password); err != nil {
+	if _, err := db.Query("INSERT INTO user_info (username, email, password) VALUES ($1,$2,$3)", username, email, password); err != nil {
 		return err
 	}
 
